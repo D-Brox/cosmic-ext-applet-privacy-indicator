7114034b6eeadfd03068672e59bee48f32a0cf55
D-Brox <davidfbg00@gmail.com>
2026-02-05 15:17:31 -0300

    chore: add icon

--- res/install.sh
+++ res/install.sh
@@ -4,3 +4,4 @@ mkdir -p ~/.local/bin/ ${XDG_DATA_HOME:-~/.local/share}/{applications,metainfo}/
 cp ./cosmic-ext-applet-privacy-indicator ~/.local/bin/
 cp ./*.desktop ${XDG_DATA_HOME:-~/.local/share}/applications/
 cp ./*.metainfo.xml ${XDG_DATA_HOME:-~/.local/share}/metainfo/
+cp ./*.svg ${XDG_DATA_HOME:-~/.local/share}/icons/hicolor/scalable/apps/
